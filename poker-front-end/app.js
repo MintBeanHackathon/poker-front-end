df06740e95e8433653b14232e3244c9b0617a858
Nuanjan <nuanjan.schluntz@gmail.com>
2021-07-29 16:38:57 -0400

    add randomCard function to show two Random card in user hand

--- poker-front-end/app.js
+++ poker-front-end/app.js
@@ -1,69 +1,88 @@
 $(document).ready(function () {
-  const path = "public/images/poker-cards/";
-  const cards = [
-    `${path}2_of_clubs.png`,
-    `${path}2_of_diamonds.png`,
-    `${path}2_of_hearts.png`,
-    `${path}2_of_spades.png`,
-    `${path}3_of_clubs.png`,
-    `${path}3_of_diamonds.png`,
-    `${path}3_of_hearts.png`,
-    `${path}3_of_spades.png`,
-    `${path}4_of_clubs.png`,
-    `${path}4_of_diamonds.png`,
-    `${path}4_of_hearts.png`,
-    `${path}4_of_spades.png`,
-    `${path}5_of_clubs.png`,
-    `${path}5_of_diamonds.png`,
-    `${path}5_of_hearts.png`,
-    `${path}5_of_spades.png`,
-    `${path}6_of_clubs.png`,
-    `${path}6_of_diamonds.png`,
-    `${path}6_of_hearts.png`,
-    `${path}6_of_spades.png`,
-    `${path}7_of_clubs.png`,
-    `${path}7_of_diamonds.png`,
-    `${path}7_of_hearts.png`,
-    `${path}7_of_spades.png`,
-    `${path}8_of_clubs.png`,
-    `${path}8_of_diamonds.png`,
-    `${path}8_of_hearts.png`,
-    `${path}8_of_spades.png`,
-    `${path}9_of_clubs.png`,
-    `${path}9_of_diamonds.png`,
-    `${path}9_of_hearts.png`,
-    `${path}9_of_spades.png`,
-    `${path}10_of_clubs.png`,
-    `${path}10_of_diamonds.png`,
-    `${path}10_of_hearts.png`,
-    `${path}10_of_spades.png`,
-    `${path}ace_of_clubs.png`,
-    `${path}ace_of_diamonds.png`,
-    `${path}ace_of_hearts.png`,
-    `${path}ace_of_spades.png`,
-    `${path}jack_of_clubs.png`,
-    `${path}jack_of_diamonds.png`,
-    `${path}jack_of_hearts.png`,
-    `${path}jack_of_spades.png`,
-    `${path}queen_of_clubs.png`,
-    `${path}queen_of_diamonds.png`,
-    `${path}queen_of_hearts.png`,
-    `${path}queen_of_spades.png`,
-    `${path}king_of_clubs.png`,
-    `${path}king_of_diamonds.png`,
-    `${path}king_of_hearts.png`,
-    `${path}king_of_spades.png`];
+  
   
   $("#deal").on('click', function() {
-    $("#cardInHandOne").attr("src", cards[randomCardIndex()]);
-    $("#cardInHandTwo").attr("src", cards[randomCardIndex()]);
+    $("#cardInHandOne").attr("src", cardInHand()[0]);
+    $("#cardInHandTwo").attr("src", cardInHand()[1]);
   })
 });
 
-function randomCardIndex() {
+// function randomCardIndex() { 
+//     const randomIndex = Math.floor(Math.random() * 52) + 1;
+//     return randomIndex;
+// }
+const path = "public/images/poker-cards/";
+let cards = [
+      `${path}2_of_clubs.png`,
+      `${path}2_of_diamonds.png`,
+      `${path}2_of_hearts.png`,
+      `${path}2_of_spades.png`,
+      `${path}3_of_clubs.png`,
+      `${path}3_of_diamonds.png`,
+      `${path}3_of_hearts.png`,
+      `${path}3_of_spades.png`,
+      `${path}4_of_clubs.png`,
+      `${path}4_of_diamonds.png`,
+      `${path}4_of_hearts.png`,
+      `${path}4_of_spades.png`,
+      `${path}5_of_clubs.png`,
+      `${path}5_of_diamonds.png`,
+      `${path}5_of_hearts.png`,
+      `${path}5_of_spades.png`,
+      `${path}6_of_clubs.png`,
+      `${path}6_of_diamonds.png`,
+      `${path}6_of_hearts.png`,
+      `${path}6_of_spades.png`,
+      `${path}7_of_clubs.png`,
+      `${path}7_of_diamonds.png`,
+      `${path}7_of_hearts.png`,
+      `${path}7_of_spades.png`,
+      `${path}8_of_clubs.png`,
+      `${path}8_of_diamonds.png`,
+      `${path}8_of_hearts.png`,
+      `${path}8_of_spades.png`,
+      `${path}9_of_clubs.png`,
+      `${path}9_of_diamonds.png`,
+      `${path}9_of_hearts.png`,
+      `${path}9_of_spades.png`,
+      `${path}10_of_clubs.png`,
+      `${path}10_of_diamonds.png`,
+      `${path}10_of_hearts.png`,
+      `${path}10_of_spades.png`,
+      `${path}ace_of_clubs.png`,
+      `${path}ace_of_diamonds.png`,
+      `${path}ace_of_hearts.png`,
+      `${path}ace_of_spades.png`,
+      `${path}jack_of_clubs.png`,
+      `${path}jack_of_diamonds.png`,
+      `${path}jack_of_hearts.png`,
+      `${path}jack_of_spades.png`,
+      `${path}queen_of_clubs.png`,
+      `${path}queen_of_diamonds.png`,
+      `${path}queen_of_hearts.png`,
+      `${path}queen_of_spades.png`,
+      `${path}king_of_clubs.png`,
+      `${path}king_of_diamonds.png`,
+      `${path}king_of_hearts.png`,
+      `${path}king_of_spades.png`];
+let randomRange = 52;
+function cardInHand() {
     
-    const randomIndex = Math.floor(Math.random() * 52) + 1;
-  
+    const randomIndex1 = Math.floor(Math.random() * randomRange + 1);
+    let randomIndex2 = Math.floor(Math.random() * randomRange + 1);
 
-    return randomIndex;
+    while(randomIndex1 === randomIndex2) {
+        randomIndex2 = Math.floor(Math.random() * randomRange + 1);   
+    }
+
+    const card1 = cards[randomIndex1];
+    const card2 = cards[randomIndex2];
+    cards[randomIndex1] = null;
+    cards[randomIndex2] = null;
+    const newCards = cards.filter(card => card !== null);
+    cards = [...newCards];
+    if(randomRange<0) return;
+    randomRange -= 2;
+    return [card1,card2];
 }
+
