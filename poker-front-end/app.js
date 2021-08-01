9149a3c551d3a7294df55410b2857e8bb002dd4a
AlexVCS <alexcurtisslep@gmail.com>
2021-08-01 09:39:34 -0400

    Added card value map variable on line 17 of app.js

--- poker-front-end/app.js
+++ poker-front-end/app.js
@@ -14,6 +14,22 @@
 
 // const SUITS = ["♠", "♣", "♥", "♦"]
 
+// const CARD_VALUE_MAP = {
+//     "2": 2,
+//     "3": 3,
+//     "4": 4,
+//     "5": 5,
+//     "6": 6,
+//     "7": 7,
+//     "8": 8,
+//     "9": 9,
+//     "10": 10,
+//     J: 11,
+//     Q: 12,
+//     K: 13,
+//     A: 14
+//   }
+
 // write function where return value from calculateValue is compared with returned
 
 // insert = (index) => {
